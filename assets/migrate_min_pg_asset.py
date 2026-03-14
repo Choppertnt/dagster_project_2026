cfe503924942863ea7055ca1bbeae3dcc070991b
chopper_test <chopper55551@gmail.com>
2026-03-14 11:09:04 +0700

    update port

--- assets/migrate_min_pg_asset.py
+++ assets/migrate_min_pg_asset.py
@@ -26,9 +26,10 @@ DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
+DB_PORT = os.getenv("DB_PORT")
 
 encoded_pass = urllib.parse.quote_plus(DB_PASS) if DB_PASS else ""
-CONN_STR = f"postgresql://{DB_USER}:{encoded_pass}@{DB_HOST}:5432/{DB_NAME}"
+CONN_STR = f"postgresql://{DB_USER}:{encoded_pass}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 embedding_model = TextEmbedding("sentence-transformers/all-MiniLM-L6-v2")
 
